3ad0637f197f32258638d13c5a4ebc8cf17e8c58
ssj71 <ssjackson71@gmail.com>
2013-06-21 11:33:06 -0600

    setting up for arpeggiator (oops)

--- src/EnvelopeFollower/envfollower.c
+++ src/EnvelopeFollower/envfollower.c
@@ -11,46 +11,13 @@
 //main functions
 LV2_Handle init_envfollower(const LV2_Descriptor *descriptor,double sample_rate, const char *bundle_path,const LV2_Feature * const* host_features)
 {
-    envfollower* synth = malloc(sizeof(envfollower));
+    envfollower* plug = malloc(sizeof(envfollower));
     unsigned char i;
 
-    synth->sample_rate = sample_rate;
+    plug->sample_rate = sample_rate;
 
-    init_waves(&(synth->waves));
-
-    synth->midi_in_p = NULL;
-    synth->nactive = 0;
-    synth->nsustained = 0;
-    synth->pitchbend = 1;
-    for(i=0;i<127;i++)
-    {
-        init_note(&(synth->note[i]),
-                  &(synth->waves),
-                  sample_rate,
-                  i,
-                  &(synth->ncells),
-                  &(synth->cell_lifetime),
-                  &(synth->amod_g),
-                  &(synth->fmod_g));
-        synth->active[i] = 0;
-        synth->sustained[i] = 0;
-    }
-
-    synth->harmonic_mode = HARMONIC_MODE_SINC;
-    for(i=0;i<MAX_N_HARMONICS;i++)
-    {
-        synth->harm_gain_sinc[i] = 1/(float)(MAX_N_HARMONICS +1);//(nharmonics+1);
-        synth->harm_gain_saw[i] = .29/(float)(i+2);//.29 makes it so gain=1 if all harmonics play
-        synth->harm_gain_sqr[i] = (i%2!=0)*.48/(float)(i+2);//odd harmonics
-        synth->harm_gain_tri[i] = (i%2!=0)*.83/(float)((i+2)*(i+2));
-    }
-    //fundamental
-    synth->harm_gain_sinc[i] = 1/(float)(MAX_N_HARMONICS +1);//(nharmonics+1);
-    synth->harm_gain_saw[i] = .29;//.29 makes it so gain=1 if all harmonics play
-    synth->harm_gain_sqr[i] = .48;
-    synth->harm_gain_tri[i] = .83;
-
-    synth->harm_gains = synth->harm_gain_sinc;
+    plug->nsum = 0;
+    plug->sum = 0;
 
     //get urid map value for midi events
     for (int i = 0; host_features[i]; i++)
@@ -60,47 +27,91 @@ LV2_Handle init_envfollower(const LV2_Descriptor *descriptor,double sample_rate,
             LV2_URID_Map *urid_map = (LV2_URID_Map *) host_features[i]->data;
             if (urid_map)
             {
-                synth->midi_event_type = urid_map->map(urid_map->handle, LV2_MIDI__MidiEvent);
+                plug->midi_event_type = urid_map->map(urid_map->handle, LV2_MIDI__MidiEvent);
+                plug->other_type = urid_map->map(urid_map->handle, LV2_ATOM__Blank);
+                plug->long_type = urid_map->map(urid_map->handle, LV2_ATOM__Long);
+                plug->float_type = urid_map->map(urid_map->handle, LV2_ATOM__Float);
+                plug->time_info_type = urid_map->map(urid_map->handle, LV2_TIME__Position);
+                plug->beatsperbar_type = urid_map->map(urid_map->handle, LV2_TIME__barBeat);
+                plug->bpm_type = urid_map->map(urid_map->handle, LV2_TIME__beatsPerMinute);
+                plug->speed_type = urid_map->map(urid_map->handle, LV2_TIME__speed);
+                plug->frame_type = urid_map->map(urid_map->handle, LV2_TIME__frame);
+                plug->framespersec_type = urid_map->map(urid_map->handle, LV2_TIME__framesPerSecond);
                 break;
             }
         }
     }
 
-    return synth;
+    return plug;
 }
 
 void connect_envfollower_ports(LV2_Handle handle, uint32_t port, void *data)
 {
-    envfollower* synth = (envfollower*)handle;
-    if(port == MIDI_IN)         synth->midi_in_p = (LV2_Atom_Sequence*)data;
-    else if(port == OUTPUT)     synth->output_p = (float*)data;
-    else if(port == CHANNEL)    synth->channel_p = (float*)data;
-    else if(port == MASTER_GAIN)synth->master_gain_p = (float*)data;
-    else if(port == RULE)       synth->rule_p = (float*)data;
-    else if(port == CELL_LIFE)  synth->cell_life_p = (float*)data;
-    else if(port == INIT_CELLS) synth->init_cells_p = (float*)data;
-    else if(port == NHARMONICS) synth->nharmonics_p = (float*)data;
-    else if(port == HARM_MODE)  synth->harmonic_mode_p = (float*)data;
-    else if(port == WAVE)       synth->wave_p = (float*)data;
-    else if(port == ENV_A)      synth->env_a_p = (float*)data;
-    else if(port == ENV_D)      synth->env_d_p = (float*)data;
-    else if(port == ENV_B)      synth->env_b_p = (float*)data;
-    else if(port == ENV_SWL)    synth->env_swl_p = (float*)data;
-    else if(port == ENV_SUS)    synth->env_sus_p = (float*)data;
-    else if(port == ENV_R)      synth->env_r_p = (float*)data;
-    else if(port == AMOD_WAV)   synth->amod_wave_p = (float*)data;
-    else if(port == AMOD_FREQ)  synth->amod_freq_p = (float*)data;
-    else if(port == AMOD_GAIN)  synth->amod_gain_p = (float*)data;
-    else if(port == FMOD_WAV)   synth->fmod_wave_p = (float*)data;
-    else if(port == FMOD_FREQ)  synth->fmod_freq_p = (float*)data;
-    else if(port == FMOD_GAIN)  synth->fmod_gain_p = (float*)data;
+    envfollower* plug = (envfollower*)handle;
+    if(port == INPUT)           plug->input_p = (float*)data;
+    else if(port == OUTPUT)     plug->output_p = (float*)data;
+    else if(port == MIDI_OUT)   plug->midi_out_p = (LV2_Atom_Sequence*)data;
+    else if(port == CHANNEL)    plug->channel_p = (float*)data;
+    else if(port == CONTROL_NO) plug->control_p = (float*)data;
+    else if(port == MINV)       plug->min_p = (float*)data;
+    else if(port == MAXV)       plug->max_p = (float*)data;
+    else if(port == PEAKRMS)    plug->peakrms_p = (float*)data;
+    else if(port == THRESHOLD)  plug->threshold_p = (float*)data;
+    else if(port == SATURATION) plug->saturation_p = (float*)data;
+    else if(port == ATIME)      plug->atime_p = (float*)data;
+    else if(port == DTIME)      plug->dtime_p = (float*)data;
     else puts("UNKNOWN PORT YO!!");
 }
 
+
+static void
+update_position(MidiFilter* self, const LV2_Atom_Object* obj)
+{
+    const MidiFilterURIs* uris = &self->uris;
+
+    // Received new transport position/speed
+    LV2_Atom *beat = NULL, *bpm = NULL, *speed = NULL;
+    LV2_Atom *fps = NULL, *frame = NULL;
+    lv2_atom_object_get(obj,
+    uris->time_barBeat, &beat,
+    uris->time_beatsPerMinute, &bpm,
+    uris->time_speed, &speed,
+    uris->time_frame, &frame,
+    uris->time_fps, &fps,
+    NULL);
+    if (bpm && bpm->type == uris->atom_Float) {
+        // Tempo changed, update BPM
+        self->bpm = ((LV2_Atom_Float*)bpm)->body;
+        self->available_info |= NFO_BPM;
+    }
+    if (speed && speed->type == uris->atom_Float) {
+        // Speed changed, e.g. 0 (stop) to 1 (play)
+        self->speed = ((LV2_Atom_Float*)speed)->body;
+        self->available_info |= NFO_SPEED;
+    }
+    if (beat && beat->type == uris->atom_Float) {
+        const double samples_per_beat = 60.0 / self->bpm * self->samplerate;
+        self->bar_beats = ((LV2_Atom_Float*)beat)->body;
+        self->beat_beats = self->bar_beats - floor(self->bar_beats);
+        self->pos_bbt = self->beat_beats * samples_per_beat;
+        self->available_info |= NFO_BEAT;
+    }
+    if (fps && fps->type == uris->atom_Float) {
+        self->frames_per_second = ((LV2_Atom_Float*)frame)->body;
+        self->available_info |= NFO_FPS;
+    }
+    if (frame && frame->type == uris->atom_Long) {
+        self->pos_frame = ((LV2_Atom_Long*)frame)->body;
+        self->available_info |= NFO_FRAME;
+    }
+}
+
+
 void run_envfollower( LV2_Handle handle, uint32_t nframes)
 {
-    envfollower* synth = (envfollower*)handle;
-    unsigned char i,j,k;
+    envfollower* plug = (envfollower*)handle;
+    u_int32_t i;
+    unsigned char j,k;
     float* buf = synth->output_p;
     LV2_Atom_Event event;
     uint32_t frame_no = 0;
@@ -121,185 +132,7 @@ void run_envfollower( LV2_Handle handle, uint32_t nframes)
 
     memset(buf,0, sizeof(float)*nframes);//start by filling buffer with 0s, we'll add to this
 
-    LV2_ATOM_SEQUENCE_FOREACH(synth->midi_in_p, event)
-    {
-        if (event && event->body.type == synth->midi_event_type)//make sure its a midi event
-        {
-            message = (unsigned char*) LV2_ATOM_BODY(&(event->body));
-            if( !(*synth->channel_p) || ((message[0]&MIDI_CHANNEL_MASK) == (*synth->channel_p)+1) )
-            {
-                type = message[0]&MIDI_TYPE_MASK;
 
-                if(type == MIDI_NOTE_ON)
-                {
-                    num = message[1]&MIDI_DATA_MASK;
-                    val = message[2]&MIDI_DATA_MASK;
-                    if(synth->note[num].note_dead == true)
-                    {
-                        synth->active[synth->nactive++] = num;//push new note onto active stack
-                    }
-                    if(firstnote)//only calculate these if there is a note in this period
-                    {
-                        firstnote = false;
-                        //condition envelope
-                        synth->envelope[ENV_ATTACK] = 1/(float)(*synth->env_a_p*synth->sample_rate);
-                        synth->envelope[ENV_DECAY] = (*synth->env_b_p-1)/(float)(*synth->env_d_p*synth->sample_rate);
-                        synth->envelope[ENV_BREAK] = *synth->env_b_p;
-                        synth->envelope[ENV_SWELL] = (*synth->env_sus_p - *synth->env_b_p)/(float)(*synth->env_swl_p*synth->sample_rate);
-                        synth->envelope[ENV_SUSTAIN] = *synth->env_sus_p;
-                        synth->envelope[ENV_RELEASE] = -(*synth->env_sus_p)/(float)(*synth->env_r_p*synth->sample_rate);
-
-                        //set harmonic gains
-                        if(*synth->harmonic_mode_p != synth->harmonic_mode)
-                        {
-                            type = synth->harmonic_mode = *synth->harmonic_mode_p;
-                            if(type == HARMONIC_MODE_SINC)
-                            {
-                                synth->harm_gains = synth->harm_gain_sinc;
-                            }
-                            else if(type == HARMONIC_MODE_SAW)
-                            {
-                                synth->harm_gains = synth->harm_gain_saw;
-                            }
-                            else if(type == HARMONIC_MODE_SQR)
-                            {
-                                synth->harm_gains = synth->harm_gain_sqr;
-                            }
-                            else if(type == HARMONIC_MODE_TRI)
-                            {
-                                synth->harm_gains = synth->harm_gain_tri;
-                            }
-                        }
-                    }
-                    if(val)
-                    {
-                        start_note(&(synth->note[num]),
-                                   &(synth->waves),
-                                   val,
-                                   event->time.frames,
-                                   synth->harm_gains,
-                                   *synth->init_cells_p,
-                                   synth->envelope);
-                    }
-                    else//velocity zero == note off
-                    {
-                        for(i=0;i<synth->nactive;i++)
-                        {
-                            if(synth->active[i] == num)
-                            {
-                                if(synth->sus)
-                                {
-                                    if(!synth->note[num].sus)
-                                    {
-                                        synth->note[num].sus = true;
-                                        synth->sustained[synth->nsustained++] = num;
-                                    }
-                                }
-                                else
-                                {
-                                    end_note(&(synth->note[num]),event->time.frames);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if(type == MIDI_NOTE_OFF)
-                {
-                    num = message[1]&MIDI_DATA_MASK;
-                    for(i=0;i<synth->nactive;i++)
-                    {
-                        if(synth->active[i] == num)
-                        {
-                            if(synth->sus)
-                            {
-                                if(!synth->note[num].sus)
-                                {
-                                    synth->note[num].sus = true;
-                                    synth->sustained[synth->nsustained++] = num;
-                                }
-                            }
-                            else
-                            {
-                                end_note(&(synth->note[num]),event->time.frames);
-                            }
-                        }
-                    }
-
-                }
-                else if(type == MIDI_CONTROL_CHANGE)
-                {
-                    num = message[1]&MIDI_DATA_MASK;
-                    if(num == MIDI_SUSTAIN_PEDAL)
-                    {
-                        val = message[2]&MIDI_DATA_MASK;
-                        if(val < 64)
-                        {
-                            synth->sus = false;
-                            //end all sus. notes
-                            for(i=0;i<synth->nsustained;i++)
-                            {
-                                if(synth->note[synth->sustained[i]].sus)
-                                {
-                                    end_note(&(synth->note[synth->sustained[i]]),event->time.frames);
-                                }
-                            }
-                            synth->nsustained = 0;
-                        }
-                        else
-                        {
-                            synth->sus = true;
-                        }
-                    }
-                    else if(num == MIDI_ALL_NOTES_OFF)
-                    {
-                        for(i=0;i<synth->nactive;i++)
-                        {
-                            num = synth->active[i];
-                            end_note(&(synth->note[num]),event->time.frames);
-
-                        }
-                        synth->nactive = 0;
-                        synth->nsustained = 0;
-                    }
-
-                }
-                else if(type == MIDI_PITCHBEND)
-                {
-                    bend = (message[1]&MIDI_DATA_MASK) + ((message[2]&MIDI_DATA_MASK)<<7) - MIDI_PITCH_CENTER;
-                    //run and update current position because this blocks (affects all notes)
-                    //run_active_notes
-                    for(j=0;j<synth->nactive;j++)
-                    {
-                        note = &(synth->note[synth->active[j]]);
-                        play_note( note,
-                                   &(synth->waves),
-                                   event->time.frames - frame_no -1,//play to frame before event
-                                   &(buf[frame_no]),
-                                   synth->pitchbend,
-                                   *synth->master_gain_p,
-                                   (unsigned char)*synth->rule_p,
-                                   *synth->wave_p,
-                                   *synth->fmod_wave_p,
-                                   fstep,
-                                   *synth->amod_wave_p,
-                                   astep);
-
-                        //cleanup dead notes
-                        if(note->note_dead)
-                        {
-                            synth->nactive--;
-                            for(k=j;k<synth->nactive;k++)
-                            {
-                                synth->active[k] = synth->active[k+1];
-                            }
-                        }
-                    }
-                    synth->pitchbend = myPow2(bend/49152);//49152 is 12*8192/2
-                    frame_no = event->time.frames;
-                }//message types
-            }//correct channel
-        }//actually midi
-    }//for each event
 
     //finish off whatever frames are left
     if(frame_no != nframes-1)
--- src/EnvelopeFollower/envfollower.h
+++ src/EnvelopeFollower/envfollower.h
@@ -4,72 +4,35 @@
 #include<lv2/lv2plug.in/ns/ext/urid/urid.h>
 #include<lv2/lv2plug.in/ns/ext/midi/midi.h>
 #include<lv2/lv2plug.in/ns/ext/atom/util.h>
+#include "lv2/lv2plug.in/ns/ext/time/time.h"
+#include "lv2/lv2plug.in/ns/ext/atom/forge.h"
 
 #define ENVFOLLOWER_URI "http://sourceforge.net/projects/infamousplugins:envfollower"
 
+typedef struct _ENVFOLLOWER{
+    float peak;
+    float rms;
 
-typedef struct _CASYNTH
-{
-    double sample_rate;
-    WAVESOURCE waves;
-    NOTE note[127];
-    unsigned char active[127];
-    unsigned char nactive;
-    bool sus;
-    unsigned char sustained[127];
-    unsigned char nsustained;
+    uint64_t nsum;
+    double sum;
 
-    double pitchbend;
+    //midi stuff
+    LV2_URID midi_event_type;
+    LV2_URID other_type;
+    //LV2_URID atom_Sequence;
+    LV2_URID float_type;
+    LV2_URID long_type;
+    LV2_URID time_info_type;
+    LV2_URID beatsperbar_type;
+    LV2_URID bpm_type;
+    LV2_URID speed_type;
+    LV2_URID frame_type;
+    LV2_URID framespersec_type;
 
-    unsigned char harmonic_mode;
-    unsigned char ncells;
-    float cell_lifetime;
-    float harm_gain_sinc[MAX_N_HARMONICS+1];
-    float harm_gain_saw[MAX_N_HARMONICS+1];
-    float harm_gain_sqr[MAX_N_HARMONICS+1];
-    float harm_gain_tri[MAX_N_HARMONICS+1];
-    float* harm_gains;//points to appropriate array
-
-    float envelope[6];
-
-    float amod_g;
-    float fmod_g;
-
-    //lv2 stuff
-    uint32_t midi_event_type;
+    LV2_Atom_Forge forge;
+    LV2_Atom_Forge_Frame frame;
 
     //ports
-    LV2_Atom_Sequence* midi_in_p;
-    float* output_p;
-
-    float* channel_p;
-
-    float* master_gain_p;
-    float* cell_life_p;
-    float* rule_p;
-    float* init_cells_p;
-    float* nharmonics_p;
-    float* harmonic_mode_p;
-    float* wave_p;
-
-    float* env_a_p;
-    float* env_d_p;
-    float* env_b_p;
-    float* env_swl_p;
-    float* env_sus_p;
-    float* env_r_p;
-
-    float* amod_wave_p;
-    float* amod_freq_p;
-    float* amod_gain_p;
-    float* fmod_wave_p;
-    float* fmod_freq_p;
-    float* fmod_gain_p;
-
-
-}CASYNTH;
-
-typedef struct _ENVFOLLOWER{
     float* input_p;
     float* output_p;
     LV2_Atom_Sequence* midi_out_p;
@@ -81,8 +44,8 @@ typedef struct _ENVFOLLOWER{
     float* peakrms_p;
     float* threshold_p;
     float* saturation_p;
-    float* aspeed_p;
-    float* dspeed_p;
+    float* atime_p;
+    float* dtime_p;
 
 
 }ENVFOLLOWER;
@@ -94,7 +57,7 @@ void connect_envfollower_ports(LV2_Handle handle, uint32_t port, void *data);
 void run_envfollower( LV2_Handle handle, uint32_t nframes);
 void cleanup_envfollower(LV2_Handle handle);
 
-static const LV2_Descriptor casynth_descriptor={
+static const LV2_Descriptor envfollower_descriptor={
     ENVFOLLOWER_URI,
     init_envfollower,
     connect_envfollower_ports,
@@ -110,34 +73,24 @@ const LV2_Descriptor* lv2_descriptor(uint32_t index)
 {
     switch (index) {
     case 0:
-        return &casynth_descriptor;
+        return &envfollower_descriptor;
     default:
         return NULL;
     }
 }
 
-enum casynth_ports
+enum envfollower_ports
 {
-    MIDI_IN =0,
+    INPUT = 0,
     OUTPUT,
+    MIDI_OUT,
     CHANNEL,
-    MASTER_GAIN,
-    RULE,
-    CELL_LIFE,//5
-    INIT_CELLS,
-    NHARMONICS,
-    HARM_MODE,
-    WAVE,
-    ENV_A,//10
-    ENV_D,
-    ENV_B,
-    ENV_SWL,
-    ENV_SUS,
-    ENV_R,//15
-    AMOD_WAV,
-    AMOD_FREQ,
-    AMOD_GAIN,
-    FMOD_WAV,
-    FMOD_FREQ,//20
-    FMOD_GAIN
+    CONTROL_NO,
+    MINV,//5
+    MAXV,
+    PEAKRMS,
+    THRESHOLD,
+    SATURATION,
+    ATIME,//10
+    DTIME
 };
