8c80d3afec6cc1f8758f50bd9646fe76005681bf
ssj71 <ssjackson71@gmail.com>
2013-09-13 22:04:27 -0600

    added input and output gain to cheap dist (untested)

--- src/cheap_distortion/plugin.h
+++ src/cheap_distortion/plugin.h
@@ -20,13 +20,15 @@ typedef void * LV2_Handle;
 #define INDEX_AUDIO_IN_1 0
 #define INDEX_AUDIO_OUT_1 1
 #define INDEX_AGGRESSION_2
+#define INDEX_INGAIN_3
+#define INDEX_OUTGAIN_4
 
 // Misc stuff for the skeleton code
 #define LV2_NUM_AUDIO_INS 1
 #define LV2_NUM_AUDIO_OUTS 1
-#define LV2_NUM_INS 1
+#define LV2_NUM_INS 3
 #define LV2_NUM_OUTS 0
-#define LV2_NUM_ALL_PORTS 3
+#define LV2_NUM_ALL_PORTS 5
 #define LV2_MIDI_OPTION 0
 #define LV2_STATE_OPTION 0
 #define LV2_EXT_OPTION 0
@@ -35,6 +37,8 @@ typedef void * LV2_Handle;
 
 // Indexes for accessing struct CHEAP_DISTORTION->In[] array
 #define LV2_AGGRESSION 0
+#define LV2_INGAIN 1
+#define LV2_OUTGAIN 2
 
 // Your plugin struct that the host passes to your DSP functions
 typedef struct {
