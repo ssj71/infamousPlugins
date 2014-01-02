c45d0dd19266944ac273d5bb0eff6a71ba17df8d
ssj71 <ssjackson71@gmail.com>
2014-01-02 09:41:20 -0700

    testing everything, s2z was misguided

--- src/spring/filtertools.c
+++ src/spring/filtertools.c
@@ -3,10 +3,12 @@
 #include<stdio.h>
 
 
+#define PI 3.1415926535897932384626433832795
+
 void cart2polar(float re, float im, float* mag, float* ang)
 {
 	*mag = sqrt(re*re + im*im);
-	*ang = atan2(re,im);
+	*ang = atan2(im,re);
 }
 
 void polar2cart(float mag, float ang, float* re, float* im)
@@ -15,6 +17,7 @@ void polar2cart(float mag, float ang, float* re, float* im)
 	*im = mag*sin(ang);
 }
 
+//need to rethink this
 void s2z(float res, float ims, float Ts, float* rez, float* imz)
 {
 	//optimizations be hanged, lets just allocate one for everything
@@ -102,12 +105,12 @@ float magnitude(float* f, uint8_t nf, float Ts, float freq_hz)
 void zpuf2filter(float* rezeros, float*imzeros,  uint8_t nzeros, float* repoles, float* impoles,  uint8_t npoles, float unityfreq, float Ts,  float* filternum, float* filterden)
 {
 	//gameplan is to convert zeros and poles to z plane, create residuals, convolve the polynomial coefficients, then set the gain to unity at the specified frequency
-	uint8_t i,n;
+	uint8_t i,nn,nd;
 	float a,b,c;
 	float poly[3];
 	float re, im;
 
-	n=1;
+	nn=1;
 	filternum[0] = 1;//start with a 1 so that polynomial starts building
 	//might need to worry about prewarp someday w' = 2/T *tan(wT/2)
 	for(i=0;i<nzeros;i++)
@@ -119,20 +122,22 @@ void zpuf2filter(float* rezeros, float*imzeros,  uint8_t nzeros, float* repoles,
 			poly[1] = -2*re;
 			poly[2] = re*re + im*im;
 			
-			convolve_in_place(poly,3,filternum,n);
-			n+=3;
+			convolve_in_place(poly,3,filternum,nn);
+			nn+=3;
+			printf("zero is %f %fi, poly is %f %f %f \n",rezeros[i],imzeros[i],poly[0],poly[1],poly[2]);
 		}
 		else
 		{//real, single zero
 			poly[0] = 1;
 			poly[1] = -re;
 			
-			convolve_in_place(poly,2,filternum,n);
-			n+=2;
+			convolve_in_place(poly,2,filternum,nn);
+			nn+=2;
+			printf("zero is %f %fi, poly is %f %f \n",rezeros[i],imzeros[i],poly[0],poly[1]);
 		}
 	}
 
-	n=1;
+	nd=1;
 	filterden[0] = 1;//start with a 1 so that polynomial starts building
 	//might need to worry about prewarp someday w' = 2/T *tan(wT/2)
 	for(i=0;i<npoles;i++)
@@ -144,37 +149,49 @@ void zpuf2filter(float* rezeros, float*imzeros,  uint8_t nzeros, float* repoles,
 			poly[1] = -2*re;
 			poly[2] = re*re + im*im;
 			
-			convolve_in_place(poly,3,filterden,n);
-			n+=3;
+			convolve_in_place(poly,3,filterden,nd);
+			nd+=3;
+			printf("pole is %f %fi, poly is %f %f %f \n",repoles[i],impoles[i],poly[0],poly[1],poly[2]);
 		}
 		else
 		{//real, single zero
 			poly[0] = 1;
 			poly[1] = -re;
 			
-			convolve_in_place(poly,2,filterden,n);
-			n+=2;
+			convolve_in_place(poly,2,filterden,nd);
+			nd+=2;
+			printf("pole is %f %fi, poly is %f %f\n",repoles[i],impoles[i],poly[0],poly[1]);
 		}
 	}
 
-
+	b = magnitude(filternum,nn,Ts,unityfreq);
+	a = magnitude(filterden,nd,Ts,unityfreq);
+	c = a/b;//inverse of magnitude
+	for(i=0;i<nn;i++)
+	{
+		filternum[i]*=c;
+	}
 }
 
 //for testing
 void main()
 {
-	float a[] = {1,1,1,1,1,1,1,1};
-	float b[20];
-	float c[20];
-	uint8_t i;
-	for(i=0;i<7;i++)
-	{
-		b[i] = 1;
-	}
-	for(;i<20;i++);
-	{	
-		b[i] = 0;
-	}
-	convolve_in_place(a,8,b,7);
-	
+
+	float a = 3;
+	float b = 0;
+	float c,d;
+	s2z(a,b,1/44100,&c,&d);
+	printf("%f %f\n",c,d);
+
+/*
+  	float rezeros[] = {-3.3011e+04,1.1251e+03};
+	float imzeros[] ={6.9032e+04,4.7615e+04};
+
+	float repoles[] = {-1.1661e+03,-5.8331e+03};
+	float impoles[] = {3.8233e+04,2.0143e+04};
+ 
+	float b[4], a[4];
+	zpuf2filter(rezeros,imzeros,2,repoles,impoles,2,20000,1/44100,b,a);
+	printf("%f %f %f %f \n%f %f %f %f\n",b[0], b[1],b[2],b[3],a[0],a[1],a[2],a[3]);
+*/
 }
