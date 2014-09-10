4b197f1516f1c696154bb88216d354d683d19b03
ssj71 <ssjackson71@gmail.com>
2014-09-10 15:01:20 -0600

    removed old cheap dist

--- src/cheap_distortion/install_lv2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-UNAME=`uname -m`
-INSTALL_DIR=/usr/local/lib/lv2/cheap_distortion.lv2
-gksu -- mkdir -p $INSTALL_DIR
-gksu -- cp -f manifest.ttl $INSTALL_DIR/manifest.ttl
-gksu -- cp -f plugin.ttl $INSTALL_DIR/cheap_distortion.ttl
-gksu -- chmod 775 $INSTALL_DIR/*.ttl
-gksu -- cp -f so.$UNAME $INSTALL_DIR/cheap_distortion.so
