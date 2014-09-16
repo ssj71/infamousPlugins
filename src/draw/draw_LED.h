2c57d397868fab6d3f4e1d0f94d0e97eff2628fa
ssj71 <ssjackson71@gmail.com>
2014-09-16 17:54:14 -0600

    added missing cas draws, made LED colors

--- src/draw/draw_LED.h
+++ src/draw/draw_LED.h
@@ -1,15 +1,38 @@
 #ifndef DRAW_BLUELED_H
 #define DRAW_BLUELED_H
-inline int cairo_code_draw_blueLED_get_width() { return 19; }
-inline int cairo_code_draw_blueLED_get_height() { return 19; }
-inline void cairo_code_draw_blueLED_render(cairo_t *cr, int val) {
+inline int cairo_code_draw_LED_get_width() { return 19; }
+inline int cairo_code_draw_LED_get_height() { return 19; }
+inline void cairo_code_draw_LED_render(cairo_t *cr, int val, unsigned char color) {
 cairo_surface_t *temp_surface;
 cairo_t *old_cr;
 cairo_pattern_t *pattern;
 cairo_matrix_t matrix;
 
+float r,g,b, rd,gd,bd;
+
+switch(color)
+{
+  case 0://red
+    r = 1; g = 0; b = 0;
+    rd = .3333; gd = 0; bd = 0;
+    break;
+  case 3://green
+    r = 0; g = 1; b = 0;
+    rd = 0; gd = .3333; bd = 0;
+    break;
+  case 4://blue
+    r = 0.3; g = 0.6; b = 1;
+    rd = 0; gd = 0; bd = .2;
+    break;
+  default://red
+    r = 1; g = 0; b = 0;
+    rd = .3333; gd = 0; bd = 0;
+    break;
+}
+
+//dark
 cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
-pattern = cairo_pattern_create_rgba(0,0.133333,0.333333,1);
+pattern = cairo_pattern_create_rgba(rd,gd,bd,1);
 cairo_set_source(cr, pattern);
 cairo_pattern_destroy(pattern);
 cairo_new_path(cr);
@@ -23,7 +46,7 @@ cairo_set_tolerance(cr, 0.1);
 cairo_set_antialias(cr, CAIRO_ANTIALIAS_DEFAULT);
 cairo_set_fill_rule(cr, CAIRO_FILL_RULE_WINDING);
 cairo_fill_preserve(cr);
- /********************/
+ /********border************/
 cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
 cairo_set_line_width(cr, 1.5);
 cairo_set_miter_limit(cr, 4);
@@ -44,11 +67,11 @@ cairo_set_antialias(cr, CAIRO_ANTIALIAS_DEFAULT);
 cairo_matrix_init(&matrix, 1,0,0,1,-60.60796,-180.04859);
 cairo_pattern_set_matrix(pattern, &matrix);
 cairo_stroke_preserve(cr);
-/********************/
+/********light************/
 if(val)
 {
 cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
-pattern = cairo_pattern_create_rgba(0,0,1,1);
+pattern = cairo_pattern_create_rgba(r,g,b,1);
 cairo_set_source(cr, pattern);
 cairo_pattern_destroy(pattern);
 cairo_new_path(cr);
@@ -69,9 +92,9 @@ cairo_set_miter_limit(cr, 4);
 cairo_set_line_cap(cr, CAIRO_LINE_CAP_BUTT);
 cairo_set_line_join(cr, CAIRO_LINE_JOIN_MITER);
 pattern = cairo_pattern_create_radial(70, 189.425, 0, 70, 189.425, 7.306019);
-cairo_pattern_add_color_stop_rgba(pattern, 0,0,0,1,1);
-cairo_pattern_add_color_stop_rgba(pattern, 0,0,0,1,1);
-cairo_pattern_add_color_stop_rgba(pattern, 0,0,0,1,0.4);
+cairo_pattern_add_color_stop_rgba(pattern, 0,r,g,b,1);
+cairo_pattern_add_color_stop_rgba(pattern, 0,r,g,b,1);
+cairo_pattern_add_color_stop_rgba(pattern, 0,r,g,b,0.4);
 cairo_matrix_init(&matrix, 0.86675,0.000000312469,-0.000000305437,0.886706,61.859452,181.110871);
 cairo_pattern_set_matrix(pattern, &matrix);
 cairo_pattern_set_extend(pattern, CAIRO_EXTEND_PAD);
@@ -93,4 +116,11 @@ cairo_stroke_preserve(cr);
 }//if on
 /********************/
 }
+
+inline void cairo_code_draw_redLED_render(cairo_t *cr, int val) {
+    inline void cairo_code_draw_LED_render(cr, val,0);}
+inline void cairo_code_draw_greenLED_render(cairo_t *cr, int val) {
+    inline void cairo_code_draw_LED_render(cr, val,3);}
+inline void cairo_code_draw_blueLED_render(cairo_t *cr, int val) {
+    inline void cairo_code_draw_LED_render(cr, val,4);}
 #endif
---
+++ src/draw/draw_casbg.h
@@ -0,0 +1,25 @@
+#ifndef DRAW_CASBG_H
+#define DRAW_CASBG_H
+inline int cairo_code_draw_casbg_get_width() { return 460; }
+inline int cairo_code_draw_casbg_get_height() { return 600; }
+inline void cairo_code_draw_casbg_render(cairo_t *cr) {
+cairo_surface_t *temp_surface;
+cairo_t *old_cr;
+cairo_pattern_t *pattern;
+cairo_matrix_t matrix;
+
+cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
+pattern = cairo_pattern_create_linear(184.28572, 485.93359, 701.42859, 678.79071);
+cairo_pattern_add_color_stop_rgba(pattern, 0,0.501961,0.501961,0.501961,1);
+cairo_pattern_add_color_stop_rgba(pattern, 0.5,0.666667,0.666667,0.666667,1);
+cairo_pattern_add_color_stop_rgba(pattern, 1,0.592157,0.592157,0.592157,1);
+cairo_matrix_init(&matrix, 1.514798,0,0,1.421429,25.714295,162.362159);
+cairo_pattern_set_matrix(pattern, &matrix);
+cairo_pattern_set_extend(pattern, CAIRO_EXTEND_PAD);
+cairo_pattern_set_filter(pattern, CAIRO_FILTER_GOOD);
+cairo_set_source(cr, pattern);
+cairo_pattern_destroy(pattern);
+cairo_paint(cr);
+/********************/
+}
+#endif
