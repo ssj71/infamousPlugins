5221f3f998fa4f1ae9fe8d124b78cca13c7b6bf6
ssj71 <ssjackson71@gmail.com>
2014-10-10 00:38:32 -0600

    it works! Need to check latency, report as necessary, but starting to turn to lushlife

---
+++ src/LushLife/retuner.h
@@ -0,0 +1,70 @@
+// -----------------------------------------------------------------------
+//
+//  Copyright (C) 2009-2011 Fons Adriaensen <fons@linuxaudio.org>
+//    
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// -----------------------------------------------------------------------
+
+#ifndef __RETUNER_H
+#define __RETUNER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef WIN32
+#include <windows.h>
+#ifndef int32_t
+#define int32_t long 
+typedef DWORD REV_ERROR;
+#endif
+#else
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+typedef int REV_ERROR;
+#endif
+
+#define TUNERTYPE_8BIT			0
+#define TUNERTYPE_16BIT			1
+#define TUNERTYPE_32BIT			2
+#define TUNERTYPE_FLOAT			3
+#define TUNERTYPE_DOUBLE			4
+#define TUNERTYPE_BITSMASK		0x07
+#define TUNERTYPE_MONOOUTPUT		0x20
+#define TUNERTYPE_MONOINPUT		0x40
+//#define TUNERTYPE_NONINTERLEAVED	0x80
+
+typedef void * TUNERHANDLE;
+
+TUNERHANDLE RetunerAlloc(int, unsigned char);
+void RetunerFree(TUNERHANDLE);
+void RetunerProcess(TUNERHANDLE, void *, void *, unsigned int);
+void RetunerSetPitch(TUNERHANDLE, float);
+void RetunerSetNoteBias(TUNERHANDLE, float);
+void RetunerSetFilter(TUNERHANDLE, float);
+void RetunerSetGain(TUNERHANDLE, float);
+void RetunerSetOffset(TUNERHANDLE, float);
+void RetunerSetNoteMask(TUNERHANDLE, unsigned int);
+unsigned int RetunerGetNoteset(TUNERHANDLE);
+float RetunerGetError(TUNERHANDLE);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
