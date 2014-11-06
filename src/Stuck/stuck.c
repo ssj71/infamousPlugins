e4b37a7ba9eb5fecff1da5dd74ed6fada5b9e025
ssj71 <ssjackson71@gmail.com>
2014-11-06 13:46:28 -0700

    just cleaning up tab size madness

--- src/Stuck/stuck.c
+++ src/Stuck/stuck.c
@@ -80,14 +80,14 @@ void run_stuck(LV2_Handle handle, uint32_t nframes)
         if(*plug->stick_it_p >= 1)
 #endif
         {
-	    plug->state = LOADING;
-	    plug->env = plug->rms_calc.rms;
-	}
+            plug->state = LOADING;
+            plug->env = plug->rms_calc.rms;
+        }
         else
-	{
+        {
             rms_block_fill(&plug->rms_calc, plug->input_p,nframes);
-	    return;
-	}
+            return;
+        }
     }
     else if(plug->state < LOADING_XFADE)
     {//decide if need to abort
@@ -97,16 +97,16 @@ void run_stuck(LV2_Handle handle, uint32_t nframes)
         if(*plug->stick_it_p < 1)
 #endif
         {
-	    //reinit
-	    plug->indx = 0;
+            //reinit
+            plug->indx = 0;
             plug->indx2 = plug->xfade_min;
-	    plug->state = INACTIVE; 
-	    plug->gain = 0; 
-	    plug->wavesize = plug->xfade_max;
-	    plug->score = 1;
+            plug->state = INACTIVE; 
+            plug->gain = 0; 
+            plug->wavesize = plug->xfade_max;
+            plug->score = 1;
             rms_block_fill(&plug->rms_calc, plug->input_p,nframes);
-	    return; 
-	} 
+            return; 
+        } 
     }
     else if(plug->state < RELEASING) 
     {//decide if released 
@@ -116,8 +116,8 @@ void run_stuck(LV2_Handle handle, uint32_t nframes)
         if(*plug->stick_it_p < 1) 
 #endif
         {
-	    plug->state = RELEASING; 
-	} 
+            plug->state = RELEASING; 
+        } 
     }
     else if(plug->state == RELEASING)
     {//decide if new trigger has been sent before release is complete
@@ -126,175 +126,175 @@ void run_stuck(LV2_Handle handle, uint32_t nframes)
 #else
         if(*plug->stick_it_p >= 1)
 #endif
-	{
+        {
             plug->state = QUICK_RELEASING; 
-	}
-	else
-	{
+        }
+        else
+        {
             rms_block_fill(&plug->rms_calc, plug->input_p,nframes);
-	}
+        }
     }
 
     for(i=0;i<nframes;)
     {    
         chunk = nframes - i;
         if(plug->state == LOADING)//load enough frames to start calculating the autocorrelation
-	{   
-	    //decide if reaching minimum length in this period
-            if(plug->indx+chunk >= plug->xfade_min+plug->acorr_size)
-	    {
-	        chunk = plug->xfade_min + plug->acorr_size - plug->indx;
-		plug->state = MATCHING;
-	    }
-	    //load buffer with compressed signal
-	    for(j=0;j<chunk;j++)
-	    {
-	        plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
-		i++;
-	    } 
-	}
-	else if(plug->state == MATCHING)//find autocorrelation
-	{
-            if(plug->indx2+chunk >= plug->xfade_max)
-	    {
-	        chunk = plug->xfade_max - plug->indx2;
-		plug->state = LOADING_XFADE;
-	    }
-	    // calculate autocorrelation of sample in buffer, save the minimum
-	    float tmp,score;
-	    for(j=0;j<chunk;j++)
-	    { 
-		score = 0;
-		t=0;
-		
-		for(k=plug->indx2;k<plug->indx2+plug->acorr_size && score<=plug->score;k++)
-		{
-		    tmp = plug->buf[k] - plug->buf[t++];//jsyk this isn't the strict definition of an autocorrelation, a variation on the principle
-		    score += tmp*tmp;
-		}
-		plug->indx2++;
-
-	        plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
-		i++;
-
-		//save place if score is lower than last minimum
-		if(score<=plug->score)
-		{
-		    plug->wavesize = plug->indx2 -1;//subtract 1 because we incremented already
-		    plug->score = score;
-		}
-	    }
-            if(plug->indx2>=plug->xfade_max)
-	    { 
-		plug->indx2 = 0;//reset indx2
-	    }
-	}
-	else if(plug->state == LOADING_XFADE)//xfade end of buffer with start (loop it) over an entire wave and fade in drone
-	{
-	    slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
-	    //decide if xfade ends in this period
-            if(plug->indx2+chunk >= plug->wavesize)
-            //if(plug->indx2+chunk >= 10)
-	    {
-		chunk = plug->wavesize - plug->indx2;
-	        //chunk = 10 - plug->indx2;
-		plug->state = PLAYING;
-	    }
-	    //decide if going to overflow
-	    if(plug->indx+chunk >= plug->bufsize)
-	    {
-	        chunk = plug->bufsize - plug->indx;
-		plug->state = XFADE_ONLY;
-	    }
-	    //load buffer with xfade
-	    for(j=0;j<chunk;j++)
-	    {
-	        plug->buf[plug->indx2] = .5*plug->buf[plug->indx2+plug->wavesize] + .5*plug->buf[plug->indx2];
-		//still loading end of buffer
-		plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
-		plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
-		plug->gain += slope; 
-	    }
+        {   
+            //decide if reaching minimum length in this period
+                if(plug->indx+chunk >= plug->xfade_min+plug->acorr_size)
+            {
+                chunk = plug->xfade_min + plug->acorr_size - plug->indx;
+                plug->state = MATCHING;
+            }
+            //load buffer with compressed signal
+            for(j=0;j<chunk;j++)
+            {
+                plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
+                i++;
+            } 
         }
-	else if(plug->state == XFADE_ONLY)//xfade after buffer is full
-	{
-	    slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
-	    //decide if xfade ends in this period
+        else if(plug->state == MATCHING)//find autocorrelation
+        {
+                if(plug->indx2+chunk >= plug->xfade_max)
+            {
+                chunk = plug->xfade_max - plug->indx2;
+                plug->state = LOADING_XFADE;
+            }
+            // calculate autocorrelation of sample in buffer, save the minimum
+            float tmp,score;
+            for(j=0;j<chunk;j++)
+            { 
+                score = 0;
+                t=0;
+                
+                for(k=plug->indx2;k<plug->indx2+plug->acorr_size && score<=plug->score;k++)
+                {
+                    tmp = plug->buf[k] - plug->buf[t++];//jsyk this isn't the strict definition of an autocorrelation, a variation on the principle
+                    score += tmp*tmp;
+                }
+                plug->indx2++;
+
+                    plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
+                i++;
+
+                //save place if score is lower than last minimum
+                if(score<=plug->score)
+                {
+                    plug->wavesize = plug->indx2 -1;//subtract 1 because we incremented already
+                    plug->score = score;
+                }
+            }
+            if(plug->indx2>=plug->xfade_max)
+            { 
+                plug->indx2 = 0;//reset indx2
+            }
+        }
+        else if(plug->state == LOADING_XFADE)//xfade end of buffer with start (loop it) over an entire wave and fade in drone
+        {
+            slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
+            //decide if xfade ends in this period
             if(plug->indx2+chunk >= plug->wavesize)
-	    {
-	        chunk = plug->wavesize - plug->indx2;
-		plug->state = PLAYING;
-	    }
-	    //xfade buffer
-	    for(j=0;j<chunk;j++)
-	    {
-	        plug->buf[plug->indx2] = .5*plug->buf[plug->indx2+plug->wavesize] + .5*plug->buf[plug->indx2];
-		plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
-		plug->gain += slope; 
-	    }
-            plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
+                //if(plug->indx2+chunk >= 10)
+            {
+                chunk = plug->wavesize - plug->indx2;
+                //chunk = 10 - plug->indx2;
+                plug->state = PLAYING;
+            }
+            //decide if going to overflow
+            if(plug->indx+chunk >= plug->bufsize)
+            {
+                chunk = plug->bufsize - plug->indx;
+                plug->state = XFADE_ONLY;
+            }
+            //load buffer with xfade
+            for(j=0;j<chunk;j++)
+            {
+                plug->buf[plug->indx2] = .5*plug->buf[plug->indx2+plug->wavesize] + .5*plug->buf[plug->indx2];
+                //still loading end of buffer
+                plug->buf[plug->indx++] = plug->input_p[i]*plug->env/rms_shift(&plug->rms_calc,plug->input_p[i]); 
+                plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
+                plug->gain += slope; 
+            }
+        }
+        else if(plug->state == XFADE_ONLY)//xfade after buffer is full
+        {
+            slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
+            //decide if xfade ends in this period
+            if(plug->indx2+chunk >= plug->wavesize)
+            {
+                chunk = plug->wavesize - plug->indx2;
+                plug->state = PLAYING;
+            }
+            //xfade buffer
+            for(j=0;j<chunk;j++)
+            {
+                plug->buf[plug->indx2] = .5*plug->buf[plug->indx2+plug->wavesize] + .5*plug->buf[plug->indx2];
+                plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
+                plug->gain += slope; 
+            }
+                plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
         }
         else if(plug->state == PLAYING)//just loop buffer and track gain changes
-	{
-	    slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
-	    for(j=0;j<chunk;j++)
-	    { 
-		plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
-		plug->gain += slope;
+        {
+            slope = (*plug->drone_gain_p-plug->gain)/(double)nframes;
+            for(j=0;j<chunk;j++)
+            { 
+                plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
+                plug->gain += slope;
                 plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
-	    }
-	}
-	else if(plug->state == RELEASING)
-	{
-	    slope = -*plug->drone_gain_p/(*plug->release_p*plug->sample_freq);
-	    //decide if released in this period
-	    if(plug->gain + chunk*slope < slope)
-	    {
-	        chunk = -plug->gain/slope; 
-		plug->state = INACTIVE; 
-	    }
-	    for(j=0;j<chunk;j++)
-	    { 
-		plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
-		plug->gain += slope; 
-                plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
-	    }
-	    if(plug->gain <= -slope)
-	    {
-	        plug->indx = 0;
-                plug->indx2 = plug->xfade_min;
-		plug->state = INACTIVE; 
-		plug->gain = 0; 
-		plug->wavesize = plug->xfade_max;
-		plug->score = 1;
-		return;
-	    }
+            }
         }
-	else if(plug->state == QUICK_RELEASING)
-	{
-	    slope = -*plug->drone_gain_p/(double)plug->xfade_min;
-	    //decide if released in this period
-	    if(plug->gain + chunk*slope < slope)
-	    {
-	        chunk = -plug->gain/slope; 
-		plug->state = LOADING; 
-	    }
-	    for(j=0;j<chunk;j++)
-	    { 
-		plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
-		plug->gain += slope;
+        else if(plug->state == RELEASING)
+        {
+            slope = -*plug->drone_gain_p/(*plug->release_p*plug->sample_freq);
+            //decide if released in this period
+            if(plug->gain + chunk*slope < slope)
+            {
+                chunk = -plug->gain/slope; 
+                plug->state = INACTIVE; 
+            }
+            for(j=0;j<chunk;j++)
+            { 
+                plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
+                plug->gain += slope; 
                 plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
-	    } 
-            rms_block_fill(&plug->rms_calc, plug->input_p,chunk);
-	    if(plug->gain <= -slope)
-	    {
-	        plug->indx = 0;
+            }
+            if(plug->gain <= -slope)
+            {
+                plug->indx = 0;
                 plug->indx2 = plug->xfade_min;
-		plug->state = LOADING; 
-		plug->wavesize = plug->xfade_max;
-		plug->score = 1;
-		plug->env = plug->rms_calc.rms;
-	    }
+                plug->state = INACTIVE; 
+                plug->gain = 0; 
+                plug->wavesize = plug->xfade_max;
+                plug->score = 1;
+                return;
+            }
+        }
+        else if(plug->state == QUICK_RELEASING)
+        {
+            slope = -*plug->drone_gain_p/(double)plug->xfade_min;
+            //decide if released in this period
+            if(plug->gain + chunk*slope < slope)
+            {
+                chunk = -plug->gain/slope; 
+                plug->state = LOADING; 
+            }
+            for(j=0;j<chunk;j++)
+            { 
+                plug->output_p[i++] += plug->gain*plug->buf[plug->indx2++];
+                plug->gain += slope;
+                plug->indx2 = plug->indx2<plug->wavesize?plug->indx2:0; 
+            } 
+            rms_block_fill(&plug->rms_calc, plug->input_p,chunk);
+            if(plug->gain <= -slope)
+            {
+                plug->indx = 0;
+                plug->indx2 = plug->xfade_min;
+                plug->state = LOADING; 
+                plug->wavesize = plug->xfade_max;
+                plug->score = 1;
+                plug->env = plug->rms_calc.rms;
+            }
         }
     }
 return;
