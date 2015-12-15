9bb0001b161e2b85fea738ff36bf6ae0d77243eb
ssj71 <ssjackson71@gmail.com>
2015-12-15 14:39:08 -0700

    trying to make bluebell happy :)

--- src/lushlife/lushlife_ui_main.cxx
+++ src/lushlife/lushlife_ui_main.cxx
@@ -323,7 +323,7 @@ static int
 resize_func(LV2UI_Feature_Handle handle, int w, int h)
 {
   LushLifeUI* self = (LushLifeUI*)handle;
-  if(w>0 && h>0)
+  if(self && w>0 && h>0)
       self->ui->size(w,h);
   
   return 0;
