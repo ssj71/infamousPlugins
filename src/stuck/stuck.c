c61151a2e276e6c819ab25e982c82ddbc835b701
ssj71 <ssjackson71@gmail.com>
2016-05-23 12:02:15 -0600

    find min is working, but maybe the wrong approach

--- src/stuck/stuck.c
+++ src/stuck/stuck.c
@@ -60,23 +60,25 @@ float findminx(float arr[])
 	//return subsample index of minima
 	float m2 = arr[3]-arr[1];
 	float a,b,c;
-	if(m2<0)
+	if(m2>0)
 	{
 		//minima is before sample
-		a = 1.5*(-arr[0] + arr[1] - 3*arr[2] + arr[3]);
+		a = 1.5*(-arr[0] + 3*arr[1] - 3*arr[2] + arr[3]);
 		b = 2*arr[0] - 5*arr[1] + 4*arr[2] - arr[3];
 		c = .5*(-arr[0] + arr[2]);
 
-		return 1-(-b+sqrt(b*b-4*a*c))/2*a;//its possible that this is wrong but I think b-.. will be out of domain t E[0,1]
+        if(!a) return 1 + c/b;
+		return 1-(-b+sqrt(b*b-4*a*c))/(2*a);//its possible that this is wrong but I think b-.. will be out of domain t E[0,1]
 	}
-	else if(m2>0)
+	else if(m2<0)
 	{
 		//minima is after sample
-		a = 1.5*(-arr[1] + arr[2] - 3*arr[3] + arr[4]);
+		a = 1.5*(-arr[1] + 3*arr[2] - 3*arr[3] + arr[4]);
 		b = 2*arr[1] - 5*arr[2] + 4*arr[3] - arr[4];
 		c = .5*(-arr[1] + arr[3]);
 
-		return (-b+sqrt(b*b-4*a*c))/2*a;//its possible that this is wrong but I think b-.. will be out of domain t E[0,1]
+        if(!a) return -c/b;
+		return (-b+sqrt(b*b-4*a*c))/(2*a);//its possible that this is wrong but I think b-.. will be out of domain t E[0,1]
 	}
 	else return 0;
 }
